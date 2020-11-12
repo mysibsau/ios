81ec5836d5c3ee765ec60b248d3f60a661b6a2ed
Artem Rylov <tema2707@icloud.com>
2020-11-12 22:22:46 +0700

    Initial project

---
+++ my-sibgu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  my-sibgu
+//
+//  Created by art-off on 12.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
